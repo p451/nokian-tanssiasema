5d4daca12a19b194a0ebbe7535029fdf9e233321
p451 <antoni.duhovgmail.com>
2025-08-19 09:50:49 +0300

    Add Google Tag Manager integration to layout component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -65,6 +66,20 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        {/* Google Tag Manager */}
+        <Script
+          id="google-tag-manager"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-K45SZJ6Q');`
+          }}
+        />
+        {/* End Google Tag Manager */}
+        
         {/* Inline kriittinen CSS nopeampaa renderöintiä varten */}
         <style dangerouslySetInnerHTML={{
           __html: `
@@ -171,6 +186,17 @@ export default function RootLayout({
         />
       </head>
       <body className="antialiased overflow-x-hidden">
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe 
+            src="https://www.googletagmanager.com/ns.html?id=GTM-K45SZJ6Q"
+            height="0" 
+            width="0" 
+            style={{display: 'none', visibility: 'hidden'}}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
+        
         {children}
       </body>
     </html>
